159a8e6454ade9afba7e04f22b03d05902cdace7
Joseph R Miles <me@josephrmiles.com>
2020-05-04 12:21:43 -0700

    Added testing to project

---
+++ src/pages/index.spec.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { render } from "@testing-library/react"
+
+import LandingPage from "./index"
+
+
+describe("<LandingPage/>", () => {
+
+    it("Fails", () => {
+        expect(false).toBe(true)
+    })
+
+    it("Passes", () => {
+        expect(true).toBe(true)
+    })
+
+})
