c7b41321ba179132c39bea3f722b57572f12dde9
Joseph R Miles <me@josephrmiles.com>
2020-05-06 08:07:54 -0700

    Added sign in/sign out functionality

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,21 @@
 import React from "react"
 
 import { useAuth0 } from "../components/Auth0Provider"
-import * as styles from './global.module.scss';
-
 import Button from "../components/Button"
 
+import * as styles from './global.module.scss';
+
 export default () => {
-  const { loginWithRedirect } = useAuth0()
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
 
   return (
     <div className={styles.Container}>
       <h1>Welcome to <strong>Marathon</strong>!</h1>
-      <Button onClick={() => loginWithRedirect({})}>Sign In</Button>
+      {
+      !isAuthenticated 
+      ? <Button onClick={() => loginWithRedirect()}>Sign In</Button>
+      : <Button onClick={() => logout()}>Sign Out</Button>
+      }
     </div>
   )
 }
