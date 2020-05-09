f34efcda0b74f780916d535404e1a76561a06e45
Joseph R Miles <me@josephrmiles.com>
2020-05-09 15:40:08 -0700

    Added querying GH, displaying repository list

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby";
 
 import { useAuth0 } from "../components/Auth0Provider"
 import Button from "../components/Button"
@@ -6,9 +7,58 @@ import Button from "../components/Button"
 import * as styles from './global.module.scss';
 
 export default () => {
-  const { isLoading, isAuthenticated, loginWithRedirect, logout, user } = useAuth0()
+  const {
+    isLoading,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user
+  } = useAuth0()
 
-  React.useEffect(() => console.log(user), [user])
+  const [ repositories, setRepositories ] = React.useState([])
+
+  React.useEffect(() => {
+    if (isAuthenticated && user !== undefined) {
+      const makeRequest = async () => {
+        const query = `
+          query {
+            viewer {
+              repositories(first: 10) {
+                edges {
+                  node {
+                    name
+                    url
+                  }
+                }
+              }
+            }
+          }
+        `
+
+        const request = {
+          user_id: user.sub,
+          query
+        }
+
+        const result = await fetch(
+          // "https://us-central1-marathon-276500.cloudfunctions.net/ghProxy",
+          "http://localhost:8080/ghProxy",
+          {
+            method: "POST",
+            body: JSON.stringify(request)
+          }
+        )
+
+        const data = await result.json()
+
+        setRepositories(
+          data.viewer.repositories.edges.map(({ node }) => node)
+        )
+      }
+
+      makeRequest()
+    }
+  }, [isAuthenticated, user])
 
   if (isLoading)
     return null
@@ -21,6 +71,12 @@ export default () => {
       ? <Button onClick={() => loginWithRedirect()}>Sign In</Button>
       : <Button onClick={() => logout()}>Sign Out</Button>
       }
+
+      <ul>
+      {repositories.map(({ name, url }) => (
+        <li key={name}><a href={url}>{name}</a></li>  
+      ))}
+      </ul>
     </div>
   )
 }
